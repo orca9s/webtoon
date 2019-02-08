4da398e64ed855ee2d9b64cf3f847735a18b0d5c
jsm <sang93423@gmail.com>
2019-02-08 15:21:24 +0900

    CustomUserModel사용 db재설정

--- app/members/admin.py
+++ app/members/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    pass
+
+
+admin.site.register(User, UserAdmin)
